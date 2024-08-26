d6e34bee5039754d9a3e4dcda039fa5c3d30749d
julien henry <julienhenry967@gmail.com>
2024-08-26 10:55:23 +0200

    chore: add jwt middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,22 @@
 import { serve } from '@hono/node-server'
 import { auth } from './routes/auth'
+import { user } from './routes/user'
+import { jwt } from 'hono/jwt'
 import { Hono } from 'hono'
 import 'dotenv/config'
 
 const app = new Hono()
 
 app.route('/auth', auth)
+app.use(
+  '/*',
+  jwt({
+    /* eslint-disable no-undef */
+    secret: process.env.JWT_SECRET ?? '',
+    cookie: 'session'
+  })
+)
+app.route('/user', user)
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
