7f1f68148faf8aee06378b0954a4c5dba0d74b7a
julien henry <julienhenry967@gmail.com>
2024-10-12 08:40:41 +0200

    chore: handle success state in context

--- front/src/context/form/authForm.tsx
+++ front/src/context/form/authForm.tsx
@@ -10,14 +10,16 @@ import { AuthPageModeEnum } from "src/types/_props";
 
 type AuthFormContextValues = {
   mode: AuthPageModeEnum;
-  isSuccess?: boolean;
   setMode: Dispatch<React.SetStateAction<AuthPageModeEnum>>;
+  isSuccess?: boolean;
+  setIsSuccess: Dispatch<React.SetStateAction<boolean>>;
 };
 
 const defaultValues: AuthFormContextValues = {
   mode: "login",
   isSuccess: false,
   setMode: () => undefined,
+  setIsSuccess: () => undefined,
 };
 
 const AuthFormContext = createContext<AuthFormContextValues>(defaultValues);
@@ -26,12 +28,14 @@ export const useAuthFormContext = () => useContext(AuthFormContext);
 
 export const AuthFormProvider: FC<PropsWithChildren> = ({ children }) => {
   const [mode, setMode] = useState<AuthPageModeEnum>("login");
+  const [isSuccess, setIsSuccess] = useState(true);
   return (
     <AuthFormContext.Provider
       value={{
         mode,
         setMode,
-        isSuccess: false,
+        isSuccess,
+        setIsSuccess,
       }}
     >
       {children}
