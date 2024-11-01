ba6492d4721b2bdc3285f376855ce8d58923f4af
julien henry <julienhenry967@gmail.com>
2024-11-01 13:12:39 +0100

    chore: add firebase

---
+++ server/src/db/firebase.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import * as admin from 'firebase-admin'
+import 'dotenv/config'
+
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_CLIENT_EMAIL
+} = process.env
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    projectId: FIREBASE_PROJECT_ID
+  }),
+  storageBucket: FIREBASE_STORAGE_BUCKET
+})
+
+export const bucket = admin.storage().bucket()
